d7f8907f35cf54761cac68f9fdaca7c5a075d069
mouhcine khairat <ton.email@example.com>
2025-11-15 00:30:26 +0100

    eureke-server

---
+++ eureka-server/src/main/java/ma/gov/pfe/EurekaServer.java
@@ -0,0 +1,16 @@
+package ma.gov.pfe;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@Slf4j
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
